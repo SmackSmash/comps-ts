c2ba49756d333e2286d98119f2d228e1c11454f9
Dan Moffat <danmoffat@gmail.com>
2025-07-16 03:13:50 +0100

    Made a basic accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,7 +1,18 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 
-const Accordion: FC<{ items: { title: string; content: string }[] }> = () => {
-  return <div>Accordion Component</div>;
+const Accordion: FC<{ items: { label: string; content: string }[] }> = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+  return (
+    <div>
+      {items.map(({ label, content }, index) => (
+        <div key={index}>
+          <h1 onClick={() => setExpandedIndex(index)}>{label}</h1>
+          {index === expandedIndex && <p>{content}</p>}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Accordion;
--- src/routes/accordion.tsx
+++ src/routes/accordion.tsx
@@ -3,9 +3,21 @@ import Accordion from '../components/Accordion';
 
 const RouteComponent = () => {
   const accordionItems = [
-    { title: 'Accordion 1', content: 'Accordion 1 content' },
-    { title: 'Accordion 2', content: 'Accordion 2 content' },
-    { title: 'Accordion 3', content: 'Accordion 3 content' }
+    {
+      label: 'Accordion 1',
+      content:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas esse minima, quo temporibus laborum reprehenderit, eaque sunt minus expedita accusantium velit quasi odio consequatur dolorum aperiam vitae nemo. Dicta, nostrum!'
+    },
+    {
+      label: 'Accordion 2',
+      content:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere at esse, culpa rem reprehenderit illum explicabo. Cumque libero voluptas, optio magni dolorem fugit ratione modi dolores et quas dolore recusandae!'
+    },
+    {
+      label: 'Accordion 3',
+      content:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad maxime aliquam illo, voluptas rem illum soluta alias cupiditate optio adipisci sed deserunt minima fuga saepe, dicta aliquid dolore repellat atque!'
+    }
   ];
 
   return (
