aa06e001b264a90e3aa38e7bc3c417c9e7cc5b04
Dan Moffat <danmoffat@gmail.com>
2025-07-16 02:01:36 +0100

    Added accordion component and sorted out props

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+const Accordion: FC<{ items: { title: string; content: string }[] }> = () => {
+  return <div>Accordion Component</div>;
+};
+
+export default Accordion;
--- src/routes/accordion.tsx
+++ src/routes/accordion.tsx
@@ -1,7 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router';
+import Accordion from '../components/Accordion';
 
 const RouteComponent = () => {
-  return <div>Hello "/accordion"!</div>;
+  const accordionItems = [
+    { title: 'Accordion 1', content: 'Accordion 1 content' },
+    { title: 'Accordion 2', content: 'Accordion 2 content' },
+    { title: 'Accordion 3', content: 'Accordion 3 content' }
+  ];
+
+  return (
+    <div className='flex gap-2 p-2'>
+      <Accordion items={accordionItems} />
+    </div>
+  );
 };
 
 export const Route = createFileRoute('/accordion')({
