68dae06789c16c83316462b26776617c2205b385
Dan Moffat <danmoffat@gmail.com>
2025-07-19 03:40:26 +0100

    Added basic header rendering

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,19 @@
 import type { FC } from 'react';
 import type { Fruit } from '../types';
 
-const Table: FC<Fruit[]> = ({ data }) => {
-  return <div>Table Component</div>;
+const Table: FC<{ data: Fruit[] }> = ({ data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(data[0]).map((key, index) => (
+            <td key={index}>{key}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
 };
 
 export default Table;
