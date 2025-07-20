3d750d482b160c912b179de9d2814e3bc22aae69
Dan Moffat <danmoffat@gmail.com>
2025-07-20 01:58:08 +0100

    Added different data to render cars

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export type Option = { label: string | number; value: string | number };
 
 export type Fruit = { name: string; color: string; score: number };
+
+export type Car = { name: string; price: number; discount: number; desirable: boolean };
