cdd0ff6f9b961dc476a78d06d5b96bb0717f51b2
Dan Moffat <danmoffat@gmail.com>
2025-07-20 01:04:49 +0100

    A little more styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,21 +5,23 @@ import { twMerge } from 'tailwind-merge';
 const Table: FC<{ data: Fruit[] }> = ({ data }) => {
   return (
     <table>
-      <thead>
+      <thead className='bg-cyan-300 text-cyan-950'>
         <tr>
           {Object.keys(data[0]).map((key, index) => (
-            <td key={index}>{key.charAt(0).toUpperCase() + key.slice(1)}</td>
+            <td key={index} className='p-2'>
+              {key.charAt(0).toUpperCase() + key.slice(1)}
+            </td>
           ))}
         </tr>
       </thead>
       <tbody>
         {data.map(({ name, color, score }, index) => (
           <tr key={index} className={index < data.length - 1 ? 'border-b-2' : ''}>
-            <td>{name}</td>
-            <td>
+            <td className='p-2'>{name}</td>
+            <td className='p-2'>
               <div className={twMerge(color, 'h-5 w-5')}></div>
             </td>
-            <td>{score}</td>
+            <td className='p-2'>{score}</td>
           </tr>
         ))}
       </tbody>
