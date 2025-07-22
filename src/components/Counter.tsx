95e04f08937b6d93a0e026760b7bcc8593752427
Dan Moffat <danmoffat@gmail.com>
2025-07-22 01:04:04 +0100

    Added effect to display count on change

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,19 @@
-import { useState, type FC } from 'react';
+import { useEffect, useState, type FC } from 'react';
 import Button from './Button';
 
 const Counter: FC<{ initialCount?: number }> = ({ initialCount = 0 }) => {
   const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
 
   const increment = () => {
     setCount(count + 1);
   };
 
   return (
-    <div className='mt-2 flex flex-col items-center gap-4'>
+    <div className='mt-2 flex flex-col items-center gap-4 rounded-2xl bg-cyan-600 p-4'>
       <div className='text-5xl font-bold'>{count}</div>
       <Button style='success' rounded onClick={increment}>
         Increment
