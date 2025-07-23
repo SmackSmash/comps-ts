2786c83bfa54a1ddad85ab076c0141223b2a5976
Dan Moffat <danmoffat@gmail.com>
2025-07-23 02:50:48 +0100

    Converted counter to use useReducer. Need to make TS happy

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,31 +1,47 @@
-import { useState, type ChangeEvent, type FC, type FormEvent } from 'react';
+import { useReducer, type ChangeEvent, type FC, type FormEvent } from 'react';
 import Button from './Button';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1 };
+    case 'decrement':
+      return { ...state, count: state.count - 1 };
+    case 'change-value':
+      return { ...state, addValue: action.payload };
+    case 'add-value':
+      return { count: state.count + action.payload, addValue: 0 };
+    default:
+      return state;
+  }
+};
+
 const Counter: FC<{ initialCount?: number }> = ({ initialCount = 0 }) => {
-  const [count, setCount] = useState(initialCount);
-  const [addValue, setAddValue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    addValue: 0
+  });
 
   const incrementCount = () => {
-    setCount(count + 1);
+    dispatch({ type: 'increment' });
   };
 
   const decrementCount = () => {
-    setCount(count - 1);
+    dispatch({ type: 'decrement' });
   };
 
   const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-    setAddValue(Number(value));
+    dispatch({ type: 'change-value', payload: Number(value) });
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setCount(count + Number(addValue));
-    setAddValue(0);
+    dispatch({ type: 'add-value', payload: Number(state.addValue) });
   };
 
   return (
     <div className='mt-2 flex flex-col items-center gap-4 rounded-2xl bg-cyan-600 p-4'>
-      <div className='text-5xl font-bold'>{count}</div>
+      <div className='text-5xl font-bold'>{state.count}</div>
       <div className='flex gap-2'>
         <Button style='success' rounded onClick={incrementCount}>
           Increment
@@ -43,7 +59,7 @@ const Counter: FC<{ initialCount?: number }> = ({ initialCount = 0 }) => {
           type='number'
           name='add'
           id='add'
-          value={addValue || ''}
+          value={state.addValue || ''}
           onChange={handleChange}
           className='rounded-2xl bg-cyan-50 px-4 py-2 outline-0'
         />
